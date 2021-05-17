7a6757ffa225de407b626822a1718aedfb5d882d
Nikita Chauhan <50770098+nikitach5@users.noreply.github.com>
2021-05-17 17:14:58 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-var userScore = 0;
-var computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
+var userScore = 0;                                         //it will store the user score
+var computerScore = 0;                                    // it will store the computer score
+
+const userScore_span = document.getElementById("user-score");            //this is will display the user-score
+const computerScore_span = document.getElementById("computer-score");    // this will display the computer score
 
 const scoreboard_div = document.querySelector(".score-board");
 const result_p = document.querySelector(".result > p");
@@ -17,11 +18,11 @@ function win(){
     result_p.innerHTML = "You win!";
 }
 
-function lose(){
+function lost(){
    computerScore++;
    computerScore_span.innerHTML = computerScore;
    userScore_span.innerHTML = userScore;
-   result_p.innerHTML = "You lose!";
+   result_p.innerHTML = "You lost!";
 }
 
 function draw(){
@@ -29,27 +30,31 @@ function draw(){
 }
 
 
-
+//  getComputerChoice function will generate the random values every time when user click on any of the three choices
 function getComputerChoice(){
     const choices = ['r', 'p', 's'];
     const randNum = Math.floor(Math.random()*3);
     return choices[randNum];
 }
 
+// game function will take the user choice and then match with the computer choice that whether it's win, lost or draw.
+// win will happen ony in three cases when user:computer choose { rock:scissor , paper:rock , scissor:paper }
+// and draw will happen when they both choose the same choice and lost in other cases.
+
 function game(userChoice){
     const computerChoice = getComputerChoice();
     if(userChoice=="r"){
         if(computerChoice=="r") draw();
-        else if(computerChoice=="p") lose();
+        else if(computerChoice=="p") lost();
         else win();
     }
     else if(userChoice=="p"){
         if(computerChoice=="r") win();
         else if(computerChoice=="p") draw();
-        else lose();
+        else lost();
     }
     else{
-        if(computerChoice=="r")  lose();
+        if(computerChoice=="r")  lost();
         else if(computerChoice=="p") win();
         else draw();
     }
